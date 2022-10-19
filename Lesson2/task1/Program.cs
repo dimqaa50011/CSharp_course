a614d24cc5bf5d6edf54805c95a0f80a5a6c98f7
Dmitry <dz5011@yandex.ru>
2022-10-19 12:06:49 +0300

    lesson2 task1

---
+++ Lesson2/task1/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Не использовать строки для
+
+int Prompt(string msg)
+{
+    System.Console.WriteLine(msg);
+    int answer = Int32.Parse(System.Console.ReadLine());
+    return answer;
+}
+
+bool ValidDigit(int number)
+{
+    if (number < 100 || number >= 1000)
+    {
+        System.Console.WriteLine($"Введенное число не являеся трехзначиным. number={number}");
+        return false;
+    }
+    return true;
+}
+
+void OnStartup()
+{
+    int num = Prompt("Введите трехзначное целое число: ");
+    if (ValidDigit(num))
+    {
+        System.Console.WriteLine((num % 100) / 10);
+    }
+}
+
+OnStartup();
