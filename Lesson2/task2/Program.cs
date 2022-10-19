ebba066c085a78264a89a8d84072ecc5f3a93dcc
Dmitry <dz5011@yandex.ru>
2022-10-19 12:21:46 +0300

    lesson2 task2

---
+++ Lesson2/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа. Не использовать строки для расчета.
+
+
+void OnStartup()
+{
+    int num = new Random().Next(100, 1000);
+    int firstDigit = num / 100;
+    int lastDigit = num % 10; 
+
+    System.Console.WriteLine($"{num} -> {firstDigit}{lastDigit}"); 
+}
+
+OnStartup();
