856ab12b46137b691b6e6808d9f0cd76eee55d9b
dimqaa50011 <86368463+dimqaa50011@users.noreply.github.com>
2022-10-19 11:17:01 +0300

    Lesson1 (#1)
    Lesson1 ready

---
+++ Lesson1/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+int num1 = Int32.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = Int32.Parse(Console.ReadLine());
+
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+
+Console.WriteLine($"num1={num1}; num2={num2} -> max={max}");
---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+System.Console.Write("Введите целое число: ");
+
+int userAnswer = Int32.Parse(System.Console.ReadLine());
+int counter = 2;
+
+string result = "";
+
+while (counter < userAnswer)
+{
+    result += $"{counter}, ";
+    counter+=2;
+}
+
+
+System.Console.WriteLine(result);
