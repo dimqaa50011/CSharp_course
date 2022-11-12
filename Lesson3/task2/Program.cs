a6f586ce67e692dc865a014708deb14216627297
dimqaa50011 <86368463+dimqaa50011@users.noreply.github.com>
2022-11-12 09:44:33 +0300

    Lesson3 (#3)
    Lesson3 ready

---
+++ Lesson3/task2/Program.cs
@@ -0,0 +1,72 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84 
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+double GetDistance(Dictionary<char, int[]> coords)
+{
+    int[] difference = new int[3];
+    double result = 0;
+
+    for (int i = 0; i < difference.Length; i++)
+    {
+        difference[i] = coords['A'][i] - coords['B'][i]; 
+    } 
+
+    for (int i = 0; i < difference.Length; i++)
+    {
+        result += Math.Pow(difference[i], 2);
+    }
+
+    return Math.Round(Math.Sqrt(result), 2);
+}
+
+Dictionary<char, int[]> GetPoints()
+{
+    char[] pointsNames = new char[2] {'A', 'B'};
+    char[] coordNames = new char[3] {'x', 'y', 'z'};
+
+    var data = new Dictionary<char, int[]>();
+    for (int i = 0; i < pointsNames.Length; i++)
+    {
+        int[] tmpArrData = new int[3];
+        for (int j = 0; j < coordNames.Length; j++)
+        {
+            int coord = Prompt($"Введите координату {coordNames[j]} для точки {pointsNames[i]}: ");
+            tmpArrData[j] = coord;
+        }
+
+        data.Add(pointsNames[i], tmpArrData);
+    }
+
+    return data;
+}
+
+void PrintResult(Dictionary<char, int[]> data, double result)
+{
+    string answer = "";
+
+    foreach (var item in data)
+    {
+        answer += $"{item.Key} ({String.Join(", ", item.Value)}) ";
+    }
+    answer += $"-> {result}";
+
+    System.Console.WriteLine(answer);
+}
+
+void Starter()
+{
+    var data = GetPoints();
+    double result = GetDistance(data);
+    PrintResult(data, result);
+}
+
+Starter();
---
+++ Lesson3/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int Prompt(string msg)
+{
+    System.Console.Write(msg);
+    return int.Parse(Console.ReadLine());
+}
+
+string GetCubeTable(int num)
+{
+    double[] cubes = new double[num];
+    for (int i = 0; i < num; i++)
+    {
+        cubes[i] = Math.Pow(i + 1, 3);
+    }
+
+    return string.Join(", ", cubes);
+}
+
+void Starter()
+{
+    int number = Prompt("Введите число: ");
+    string cubes = GetCubeTable(number);
+    System.Console.WriteLine($"{number} -> {cubes}");
+}
+
+Starter();
